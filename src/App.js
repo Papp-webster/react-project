9703f0a422eaddc216b65e1d3ec67f1f504beec1
Papp László <papp.lazslo.web@gmail.com>
2020-07-02 10:27:32 +0200

    person

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Person from "./Person/person";
 function App() {
   return (
     <div className="App">
       <h1>React app!</h1>
+      <p>This is working!</p>
+      <Person />
     </div>
   );
 }
