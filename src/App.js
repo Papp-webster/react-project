e761ddbd3854c571b17f7897dd5e37b313fe96f5
Papp László <papp.lazslo.web@gmail.com>
2020-07-02 11:42:53 +0200

    Change names

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/person";
-function App() {
+
+const App = (props) => {
+  const [person, setPersons] = useState({
+    persons: [
+      { name: "Max", age: "22" },
+      { name: "Manu", age: "18" },
+      { name: "Lucy", age: "28" },
+    ],
+    other: "something",
+  });
+  const switchNameHandler = () => {
+    setPersons({
+      persons: [
+        { name: "Zoli", age: "33" },
+        { name: "Pisti", age: "36" },
+        { name: "Géza", age: "22" },
+      ],
+    });
+  };
   return (
     <div className="App">
       <h1>React app!</h1>
       <p>This is working!</p>
-      <Person />
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person name={person.persons[0].name} age={person.persons[0].age} />
+      <Person name={person.persons[1].name} age={person.persons[1].age}>
+        Hobbies : Skateboarding
+      </Person>
+      <Person name={person.persons[2].name} age={person.persons[2].age} />
     </div>
   );
-}
+};
 
 export default App;
