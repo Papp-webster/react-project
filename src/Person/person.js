3e639cc8453b7c122786f6abcb59e66604b7befb
Papp László <papp.laszlo.web@gmail.com>
2020-08-01 16:37:09 +0200

    delete button

--- src/Person/person.js
+++ src/Person/person.js
@@ -7,7 +7,7 @@ const person = (props) => {
       <p onClick={props.click}>
         I'm {props.name} and im {props.age} year old
       </p>
-      <p>{props.children}</p>
+       <button className = "gomb" onClick = {props.click}> Delete </button> 
       <input id="name" type="text" onChange={props.changed} value={props.currentName}/>
      
     </div>
